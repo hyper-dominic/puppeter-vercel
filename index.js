9409a46f2ea58c673b28c531b137a8d9c74ce44f
Sam Shubham <68236217+sam-shubham@users.noreply.github.com>
2023-02-02 23:38:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   puppeteer = require("puppeteer");
 }
 
-app.get("/api/getmovie", async (req, res) => {
+app.get("/api/codeBlock", async (req, res) => {
   let options = {};
 
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
@@ -24,47 +24,64 @@ app.get("/api/getmovie", async (req, res) => {
   }
 
   try {
-    let id = req.query.tmdb;
-    console.log(id);
+    
+    let {language , code , width , bg ,pv,ph,theme } = req.query
+    
+    
+    console.log({language , code , width , bg ,pv,ph,theme});
+    
+   let terminalConfig = {
+  width: width||"680",
+  bg: bg||"rgba(171, 184, 195, 1)",
+  theme: theme||"seti",
+  paddingVertical: pv||"56",
+  paddingHorizontal: ph||"56",
+};
+
+let CodeBlock = {
+  language: language||"javascript",
+  code: code||`console.log("Its Me Shubham")`,
+};
+    
+    const url = `https://carbon.now.sh/?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=${CodeBlock.language}&width=${terminalConfig.width}&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=${terminalConfig.paddingVertical}px&ph=${terminalConfig.paddingHorizontal}px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=4x&wm=false&code=${CodeBlock.code}`;
+
 
     // Configures puppeteer
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-    console.log("page", page);
+//     console.log("page", page);
     // await page.setUserAgent(
     //   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
     // );
-    await page.goto(`https://www.2embed.to/embed/tmdb/movie?id=${id}`, {
+    await page.goto(url, {
       timeout: 30000,
       waitUntil: "load",
     });
 
-    await page.setRequestInterception(true);
+//     await page.setRequestInterception(true);
     page.on("request", async (request) => {
       console.log("request", request);
-      if (request.url().includes("https://www.2embed.to/ajax/embed/play")) {
-        let reqUrl = request.url();
-        let token = reqUrl.split("&_token=")[1].split("&")[0];
-        console.log("token", token);
+      
+         if (r.url().includes("data:image/")) {
+           
+           let image = r.url();
+           res.send({ image });
 
-        // console.log(req.query.tmdb);
-        // console.log(token);
-
-        await request.abort();
         await browser.close();
-        res.send({ token });
-      } else {
-        await request.continue();
-      }
+         }
+        
+      
     });
+    
+    const source = await page.$(".jsx-2184717013");
 
-    await page.waitForSelector("#play-now");
-    const play = await page.$("#play-now");
+//     await page.waitForSelector("#play-now");
+//     const play = await page.$("#play-now");
 
     // console.log(play);
-    await play.click();
-    await page.waitForTimeout(1);
-    await play.click();
+    source.click();
+//     await page.waitForTimeout(1);
+//     await play.click();
   } catch (e) {
     console.error("error mine", e);
     res.send("Something Went Wrong");
